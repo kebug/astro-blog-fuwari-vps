a48db56e85f95eeb6a6b6d39e33f7e6bf42aa897
kebug666 <kebug666@gmail.com>
2025-10-21 15:18:01 +0000

    change astro config base

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://kebug.github.io/",
-	base: "/",
+	base: "/astro-blog-fuwari-vps",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
